aa55e4870a9359bb57b5738f8254df12f24fa3ec
Reinaldo Silva <reinaldo.silva.jr@outlook.com>
2024-09-14 17:35:00 -0300

    add animation root page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
+import { ClientOnly } from "@/components/ClientOnly";
 import { Logo } from "@/components/Logo";
 import { ShortenUrlForm } from "@/components/ShortenUrlForm";
 
 export default function Home() {
   return (
     <div className="bg-zinc-50 h-svh flex flex-col items-center justify-center">
-      <Logo />
+      <ClientOnly
+        fallback={
+          <div className="animate-pulse bg-zinc-300 rounded-xl w-[138px] h-[42px]" />
+        }
+      >
+        <Logo whitAnimation />
+      </ClientOnly>
       <p className="text-zinc-500 mt-2">Shorten and manage your URLs easily.</p>
       <ShortenUrlForm />
     </div>
--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,12 +1,52 @@
+"use client";
+import { animated, useSpring } from "@react-spring/web";
+import clsx from "clsx";
 import { Link2 } from "lucide-react";
+import { useState } from "react";
 
-export function Logo() {
+interface LogoProps {
+  whitAnimation?: boolean;
+}
+
+export function Logo({ whitAnimation = false }: LogoProps) {
+  const [showLinky, setShowLinky] = useState(false);
+
+  const url =
+    "https://www.thisisthemostridiculouslylongurlthatevenyourcatwouldgetboredreading.com/seriously/whywouldanyone/needalink/thislong/areyoustillthere/becauseitisgettingweird/justclickalready/pleasedontleave/ohwaityouarestillreading";
+
+  const fullText = showLinky ? "Linky!" : url;
+  const textLength = fullText.length;
+
+  const { animatedText } = useSpring({
+    to: { animatedText: textLength },
+    from: { animatedText: 0 },
+    reset: true,
+    reverse: showLinky,
+    delay: 500,
+    onRest: () => setShowLinky(true),
+    config: { duration: showLinky ? 3000 : 2000 },
+  });
+
   return (
-    <div className="flex items-end mx-auto select-none">
+    <div className="flex items-end mx-auto select-none overflow-x-hidden">
       <div className="border-[3px] border-zinc-800 rounded-xl p-1">
         <Link2 size={28} color="black" className="-rotate-45" strokeWidth={3} />
       </div>
-      <h1 className="font-sans text-3xl font-semibold ml-2">Linky!</h1>
+      <animated.h1
+        className={clsx(
+          "ml-2 truncate flex-1 text-ellipsis max-w-[260px] sm:max-w-md md:max-w-lg lg:max-w-xl xl:max-w-3xl",
+          {
+            "text-xl font-mono": !showLinky && whitAnimation,
+            "font-sans text-3xl font-semibold ": showLinky || !whitAnimation,
+          }
+        )}
+      >
+        {whitAnimation
+          ? animatedText.to((val) =>
+              fullText.slice(0, textLength - Math.floor(val))
+            )
+          : "Linky!"}
+      </animated.h1>
     </div>
   );
 }
