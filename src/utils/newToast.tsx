5e4ee06e21a644d08e1d0f69ed6472427181149d
Reinaldo Silva <reinaldo.silva.jr@outlook.com>
2025-01-17 17:04:34 -0300

    change toast lib

--- src/utils/newToast.tsx
+++ src/utils/newToast.tsx
@@ -1,15 +1,28 @@
-import { toast, TypeOptions } from "react-toastify";
+import toast, { ToastOptions } from "react-hot-toast";
 
 interface NewToastProps {
-  type?: TypeOptions;
+  type?: "success" | "error";
   text: string;
 }
 
 export function newToast({ text, type = "success" }: NewToastProps) {
-  toast(text, {
+  const configs: ToastOptions = {
     position: "bottom-center",
-    type,
-    autoClose: 4000,
-    closeOnClick: true,
-  });
+    duration: 6000,
+    style: {
+      border: "2px solid #27272a",
+      padding: "12px",
+      color: "#27272a",
+    },
+    iconTheme: {
+      primary: "#27272a",
+      secondary: "#FFFAEE",
+    },
+  };
+
+  if (type === "success") {
+    return toast.success(text, configs);
+  }
+
+  return toast.error(text, configs);
 }
